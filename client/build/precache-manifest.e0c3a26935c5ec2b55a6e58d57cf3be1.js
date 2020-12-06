83100f74f36f2234a5fc6a3e60c21c42ebf73c74
Cynthia Wang <cynthia@ibm.com>
2020-12-05 22:54:59 -0700

    npm i react scripts

---
+++ client/build/precache-manifest.e0c3a26935c5ec2b55a6e58d57cf3be1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3862757b2729c88b9c3e8febbac1c244",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ca2111270856587c7ce",
+    "url": "/static/css/main.5c6d18c7.chunk.css"
+  },
+  {
+    "revision": "fc99c9b0c8eaee64e941",
+    "url": "/static/js/2.17cbaf0e.chunk.js"
+  },
+  {
+    "revision": "1ca6cdac1a389db5f19a7acd43f6ee93",
+    "url": "/static/js/2.17cbaf0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ca2111270856587c7ce",
+    "url": "/static/js/main.4adb7f52.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
