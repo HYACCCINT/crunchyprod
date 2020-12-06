b9f99779d9ff18a4c862c2db1b9a33d30ff120cd
Cynthia Wang <cynthia@ibm.com>
2020-12-05 22:23:21 -0700

    npm i react scripts

--- server/src/server.ts
+++ server/src/server.ts
@@ -43,8 +43,14 @@ app.use(cors({
 app.use(cookieParser());
 const path = require('path');
 
-const staticFiles = express.static(path.join(__dirname, '../../client/build'))
-app.use(staticFiles)
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, '../../client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
+  });
+}
 
 
 /**
